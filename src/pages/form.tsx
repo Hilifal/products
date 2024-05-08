764c5bb8c3c3203a9f964fc8523592a347c2cdcb
Евгений Губанков <gea@vt2b.ru>
2024-05-08 16:22:32 +0500

    сделал добавление токена

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,27 +1,53 @@
+import axios from "axios"
 import Link from "next/link"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 
 function Form(){
+	//const axios = require('axios')
+	const [token, setToken] = useState('токена нет')
 	const {
+		//регистрирует поля для формы
 		register,
+		//позволяет реализовать валидацию и тд  (условный onSubmit не будет вызван если форма заполнена некорректно)
 		handleSubmit,
-		watch,
+		//watch,
 		formState: { errors },
 	} = useForm()
-	const onSubmit: any = (data:any) => console.log(data)
-
-	console.log(watch("example"))
+	const onSubmit: any = (data:any) => {
+		console.log(data)
+		axios.post('https://mplace-backend.dev-vt2b.ru/auth/login', {
+			email: data.email,
+			password: data.password
+		})
+		.then(response => {
+			console.log('Response:', response.data)
+			setToken(response.data.access_token)
+		})
+		.catch(error => {console.error('Error:', error)})
+		
+	}
+	console.log(errors)
+	//console.log(watch("example"))
 
 	return (
 		<>
 			<Link href={'/'}>К товарам</Link>
 			<Link href={'/cart'}>В корзину</Link>
 			<form onSubmit={handleSubmit(onSubmit)}>
-				<input defaultValue={"test"} {...register("exsmple")} />
-				<input {...register("exampleRequred", {required: true})} />
-				{errors.exampleRequired && <span>This field is required</span>}
+				<label>
+					Введите Email:
+					<input defaultValue={"test@test.ru"} {...register("email")} />
+				</label>
+				{/*необходимо передать 2 параметра: 1) уникальное имя для инпута 2) объект с различными параметрами для валидации*/}
+				<label>
+					Введите пароль:
+					<input {...register("password", {required: true})} />
+				</label>
+				{errors.password && <span>This field is required</span>}
 				<input type="submit" />
 			</form>
+			<div>{token}</div>
 		</>
 	)
 
